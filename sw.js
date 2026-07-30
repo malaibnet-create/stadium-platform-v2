408608041e6ad3bf69eae48bd9c1df488a986258
malaibnet-create <malaib.net@gmail.com>
2026-07-30 10:07:27 +0100

    Change cache name and handle notification clicks
    Updated cache name and added notification click event handler.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'malaeb-net-v4-security';
+aconst cacheName = 'malaeb-net-v5-reminders';
 const assets = [
   './',
   './index.html',
@@ -65,6 +65,18 @@ self.addEventListener('fetch', e => {
         await cache.put(e.request, networkResponse.clone());
       }
       return networkResponse;
+    })
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+      for (const client of clientList) {
+        if ('focus' in client) return client.focus();
+      }
+      return clients.openWindow('./');
     })
   );
 });
